bb248f4b2437dcc2b8da00e7100aee490febacf5
Radu Sabau <radu.sabau@analog.com>
2025-02-20 14:12:31 +0200

    projects: PID_app : Add Hardware-In-The-Loop support with Simulink
    Add Simulink Schematic, Block Diagram and MATLAB scripts included for HIL.
    Make changes to project for optimization and HIL support.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/PID_app/src/platform/stm32/parameters.h
+++ projects/PID_app/src/platform/stm32/parameters.h
@@ -40,30 +40,14 @@ extern UART_HandleTypeDef		huart3;
 
 #define GPIO_OPS			&stm32_gpio_ops
 #define GPIO_EXTRA			&pid_gpio_extra
-#define PWM_GPIO_EXTRA			&pid_pwm_gpio_extra
 
 #define PWM_SECTOR_ID			1
 #define PWM_SECTOR_PERIOD_NS		20000
 #define PWM_OPS				&stm32_pwm_ops
 
-#define PWM_UH_EXTRA			&pid_pwm_uh_extra
-#define PWM_VH_EXTRA			&pid_pwm_vh_extra
-#define PWM_WH_EXTRA			&pid_pwm_wh_extra
-
-#define PWM_UL_EXTRA			&pid_pwm_ul_extra
-#define PWM_VL_EXTRA			&pid_pwm_vl_extra
-#define PWM_WL_EXTRA			&pid_pwm_wl_extra
-
 extern struct stm32_uart_init_param pid_uart_extra;
 extern struct stm32_gpio_init_param pid_gpio_extra;
-extern struct stm32_gpio_init_param pid_pwm_gpio_extra;
 extern struct stm32_spi_init_param pid_spi_extra_tmc4671;
 extern struct stm32_spi_init_param pid_spi_extra_tmc6100;
-extern struct stm32_pwm_init_param pid_pwm_uh_extra;
-extern struct stm32_pwm_init_param pid_pwm_vh_extra;
-extern struct stm32_pwm_init_param pid_pwm_wh_extra;
-extern struct stm32_pwm_init_param pid_pwm_ul_extra;
-extern struct stm32_pwm_init_param pid_pwm_vl_extra;
-extern struct stm32_pwm_init_param pid_pwm_wl_extra;
 
 #endif /* __PARAMETERS_H__ */
