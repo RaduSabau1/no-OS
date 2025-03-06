1f13d1299bd17c3361d926ca60f5e57b386b6d87
Radu Sabau <radu.sabau@analog.com>
2025-03-06 13:47:25 +0200

    projects: PID_App : Remove unnecessary examples and platforms.
    Remove open_loop example support and pre-requisites for int ol support.
    Remove maxim platform support(deprecated).
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/PID_app/src/examples/examples_src.mk
+++ projects/PID_app/src/examples/examples_src.mk
@@ -1,11 +1,2 @@
-ifeq (y, $(strip $(EXT_LOOP_EXAMPLE)))
-CFLAGS += -DEXT_LOOP_EXAMPLE
 SRCS += $(PROJECT)/src/examples/ext_loop/ext_loop_example.c
 INCS += $(PROJECT)/src/examples/ext_loop/ext_loop_example.h
-endif
-
-ifeq (y, $(strip $(OPEN_LOOP_EXAMPLE)))
-CFLAGS += -DOPEN_LOOP_EXAMPLE
-SRCS += $(PROJECT)/src/examples/open_loop/open_loop_example.c
-INCS += $(PROJECT)/src/examples/open_loop/open_loop_example.h
-endif
