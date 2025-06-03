6390103ab43f8cf5a5f39c09a1f5b1384ca169ea
Radu Sabau <radu.sabau@analog.com>
2025-06-03 14:52:13 +0300

    projects: PID_App : Release v2.0 of APP
    Improve TORQUE and FLUX readings in WEB APP.
    Improve schematic, add scale for Torque and Flux Simulation Signals.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/PID_app/src/platform/platform_includes.h
+++ projects/PID_app/src/platform/platform_includes.h
@@ -1,10 +1,6 @@
 #ifndef __PLATFORM_INCLUDES_H__
 #define __PLATFORM_INCLUDES_H__
 
-#ifdef MAXIM_PLATFORM
-#include "maxim/parameters.h"
-#endif
-
 #ifdef STM32_PLATFORM
 #include "stm32/parameters.h"
 #endif
